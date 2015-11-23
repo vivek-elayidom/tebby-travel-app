45398d51b1275a08d6b89189c1134ac05efdb2dd
Sajan Mullappally <mullappallysajan@gmail.com>
2015-11-23 10:09:59 +0530

    feed-controller

---
+++ assets/js/controllers/feed.js
@@ -0,0 +1,28 @@
+var app = angular.module('app',[]);
+
+app.controller('FeedController', function ($scope) {
+  $scope.initFeed = function () {
+    $('#gallery-feed .grid-item').each(function() {
+      var gridHeight = $('#gallery-feed .one-three').width();
+      $('#gallery-feed .one-three').height(gridHeight);
+      $('#gallery-feed .two-three').height(gridHeight);
+      $('#gallery-feed .two-two').height((gridHeight*2)+10);
+    });
+    $('#gallery-feed .img-hldr').each(function () {
+      if ($(this).find('img').length){
+        var getImage = $(this).find('img').attr('src');
+        $(this).css({"background-image":"url("+getImage+")"});
+        $(this).find('img').hide();
+      }
+    });
+    $(".photo-grid").mCustomScrollbar({
+      theme:"dark-thin"
+    });
+  }
+  $scope.initFeed();
+})
+.directive('feed', function() {
+  return {
+    templateUrl: 'views/feed.html'
+  };
+});
